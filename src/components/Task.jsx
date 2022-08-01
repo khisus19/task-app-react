9ef10832f14d6ae6a1240605f3938ac17c113f31
Jesús Álvarez <khisus19@gmail.com>
2022-08-01 17:29:52 -0500

    Task comp done

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react';
 import '../styles/Task.css';
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 
-const Task = ({ text }) => {
+const Task = ({ text, done }) => {
   return (
-    <div className="task-container">
+    <div className={done ? 'task-container done' : 'task-container'}>
       <div className='task-text'>
         {text}
       </div>
-      <div className='task-icon'>
-        Eliminar
+      <div className='task-icon-container'>
+        <AiOutlineCloseCircle className='task-icon' />
       </div>
     </div>
   )
